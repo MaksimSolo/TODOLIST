a7bef2d62be7943eee3aef6bac859624fdd341bb
MaksimSolo <taezhnyi@gmail.com>
2023-03-07 22:24:58 +0300

    refactor: minimize boilerplate code with redux toolkit in auth reducer

--- src/app/store/auth-reducer.ts
+++ src/app/store/auth-reducer.ts
@@ -5,24 +5,25 @@ import {setAppStatusAC} from "./app-reducer";
 import {authAPI} from "../api/todolist-api";
 import {clearStateData} from "./todolists-reducer";
 import {LoginParamsType, ResponseResultCode} from "../../common/types/types";
+import {createSlice, PayloadAction, Reducer} from "@reduxjs/toolkit";
 
 const initialState = {
   isLoggedIn: false
 }
 
-export const authReducer = (state: AuthInitialStateType = initialState, action: LoggedInActionType): AuthInitialStateType => {
-  switch (action.type) {
-    case 'login/SET-IS-LOGGED-IN':
-      return {
-        ...state, isLoggedIn: action.value
-      }
-    default:
-      return state;
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: initialState,
+  reducers: {
+    setIsLoggedIn: (state: AuthStateType, action: LoggedInActionType) => {
+      state.isLoggedIn = action.payload
+    }
   }
-};
+})
 
-//action-creator
-export const setIsLoggedIn = (value: boolean) => ({type: 'login/SET-IS-LOGGED-IN', value} as const)
+export const authReducer: Reducer<AuthStateType, LoggedInActionType> = authSlice.reducer
+
+export const {setIsLoggedIn} = authSlice.actions
 
 //thunk -creator
 export const loginTC = (loginParams: LoginParamsType): AppThunk => async dispatch => {
@@ -60,5 +61,5 @@ export const logoutTC = (): AppThunk => async dispatch => {
 
 
 //types
-export type AuthInitialStateType = typeof initialState;
-export type LoggedInActionType = ReturnType<typeof setIsLoggedIn>
+export type AuthStateType = typeof initialState;
+export type LoggedInActionType = PayloadAction<boolean>
